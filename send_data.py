886ed2104bed83748fd197d959e11904a3dacfe3
Phoenix-VR <86906691+Phoenix-VR@users.noreply.github.com>
2023-06-14 02:30:49 +0530

    Update send_data.py

--- send_data.py
+++ send_data.py
@@ -2,7 +2,7 @@ import socket
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
-sock.bind(('0.0.0.0',5656))
+sock.bind(('0.0.0.0',5657))
 
 addr = str(input("ip:"))
 port = int(input("port:"))
