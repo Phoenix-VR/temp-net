66f4e8ce7deb648513d7981cdc110968bf9fd0a5
Phoenix-VR <86906691+Phoenix-VR@users.noreply.github.com>
2023-07-07 14:46:34 +0530

    Update api.py

--- api.py
+++ api.py
@@ -2,14 +2,45 @@ import socket
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock.bind(('0.0.0.0',80))
-connections = []
+
 print("Listening for connection requests")
 sock.listen()
+
+connections = {}
+addresses = []
+
 while True:
     client_sock, client_addr = sock.accept()
+
     print("Request from:",client_addr)
-    req_data = client_sock.recv(1024)
-    print(req_data)
-    response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n" + "<html><head><title>Demo</title></head><body><h1>Hello</h1></body></html>"
-    client_sock.sendall(response.encode())
-    client_sock.close()
+    data = client_sock.recv(1024).decode()
+    recv_data = data.split(":")
+    response = ""
+    if recv_data[0] == "register":
+        client_ip,client_port = recv_data[1],recv_data[2]
+        client_addr = str(client_ip+":"+client_port)
+        if len(connections.keys()) != 0:
+            if client_addr not in connections:
+                connections[client_addr] = client_sock
+                addresses.append(client_addr)
+                response = "registered success"
+            else:
+                response = "you have already registered please wait for other peer to connect"
+        else:
+            connections[client_addr] = client_sock
+            addresses.append(client_addr)
+            response = "registered success"
+    
+    if len(connections.keys()) == 1:
+        client_sock.sendall(response.encode())
+    elif len(connections.keys()) == 2:
+        # we can finally tell the 2 connected peers when to initiate the hole punching
+        for i,client in enumerate(connections.keys()):
+            connections[client].sendall("Initiate".encode())
+            if i == 0:
+                connections[client].sendall(addresses[1].encode())
+            elif i == 1:
+                connections[client].sendall(addresses[0].encode())
+            connections[client].close()
+        connections.clear()
+# recieve network address of that thing 
